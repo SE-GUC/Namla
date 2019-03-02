5d82e53af5ac02e2ffce334a7d12a702b5bb6992
SeifYasser5 <seifyasser@icloud.com>
2019-03-02 23:56:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser');
 const TeamsController = require('./routes/api/TeamsController');
+const Carts = require('./routes/api/Carts')
 
-const app = express();
+const app = express()
+app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+
+// Direct routes to appropriate files 
+
+app.use('/api/Carts', Carts)
+
+//Route to teamcontroller
 app.use('/api/teams', TeamsController);
-app.listen(3000, () => console.log('SEVER RUNNING'));
+
+// Handling 404
+app.use((req, res) => {
+    res.status(404).send({err: 'We can not find what you are looking for'});
+ })
+
+const port = 3000
+app.listen(port, () => console.log(`Server up and running on port ${port}`))
