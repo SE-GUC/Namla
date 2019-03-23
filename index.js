e552f3c69f314f6d9a0bf672e909165d24990441
SeifYasser5 <seifyasser@icloud.com>
2019-03-23 23:01:52 +0200

    Sprint2

--- index.js
+++ index.js
@@ -1,109 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-
-
-// Require Router Handlers
-const Announcements = require('./routes/api/Announcement')
-
-
+const uri = require('./config/keys_dev.js')
+const TeamsController = require('./routes/api/TeamsController')
 const app = express()
 
-// DB Config
-const db = require('./config/keys').mongoURI
-
-// Connect to mongo
-mongoose
-    .connect(db)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.log(err))
-
-
-
-const products = require('./routes/api/products')
-
-
-
-
-
-const RecruitmentForms = require('./routes/api/RecruitmentForms')
-
-const Carts = require('./routes/api/Carts')
-const admins = require('./routes/api/admins')
-const users = require('./routes/api/users')
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-
-const app = express()
-
-
+mongoose.connect(uri.mongoURI)
+.then(() => console.log('Mongodb connected'))
+.catch( err => console.log(err))
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-
-// Entry point
-app.get('/', (req,res) => res.send(`<h1>Book Store</h1>`))
-app.get('/test', (req,res) => res.send(`<h1>Deployed on Heroku</h1>`))
-=======
-
-
-// Direct to Route Handlers
-app.use('/api/Announcement', Announcements)
-=======
-app.get('/gallery', (req, res) => {
-    res.send(`<h1>Welcome to the gallery</h1>
-    <a href="/api/users">Users</a>
-    <a href="/api/admins">admins</a>
-    `);
-  })
-
-
-const db = require('./config/keys').mongoURI
-
- // Connect to mongo
- mongoose
-     .connect(db)
-     .then(() => console.log('Connected to MongoDB'))
-     .catch(err => console.log(err))
-
-
- app.use(express.json())
- app.use(express.urlencoded({extended: false}))
-
-
-
-app.get('/product', (req, res) => {
-    res.send(`<h1>Welcome to our store</h1>
-    `);
-})
- app.use('/api/products', products)
-
-
-app.use('/api/RecruitmentForms', RecruitmentForms)
-
-
-app.use((req,res) => res.status(404).send(`<h1>Can not find what you're looking for</h1>`))
-
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Server on ${port}`))
-
-app.use('/api/admins', admins)
-app.use('/api/users', users)
-app.use('/api/Admins', Admins)
-
+//Route to teamcontroller
+app.use('/api/teams', TeamsController);
 
 // Handling 404
 app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});
  })
 
-
-
-
-// Define the port, get it from the enviroment (used in production)
-// Or just use 3000
-const port = process.env.PORT | 3000
+const port = 3000
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
-
