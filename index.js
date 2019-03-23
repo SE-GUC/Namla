27915ac24a76ddd8db0fc8be9e91212e468b38d0
marwanelshantaly <aya.bayoumy@student.guc.edu.eg>
2019-03-23 20:50:09 +0200

    commited

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const express = require('express')
-const path = require('path');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+
+// Require Router Handlers
+const Announcements = require('./routes/api/Announcement')
+
+
+const app = express()
+
+// DB Config
 const db = require('./config/keys').mongoURI
 
+// Connect to mongo
 mongoose
     .connect(db)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err))
 
-const Admins = require('./routes/api/Admins')
-const RecruitmentForms = require('./routes/api/RecruitmentForms')
-const Carts = require('./routes/api/Carts')
-
-const app = express()
+// Init middleware
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+// Entry point
+app.get('/', (req,res) => res.send(`<h1>Book Store</h1>`))
+app.get('/test', (req,res) => res.send(`<h1>Deployed on Heroku</h1>`))
+
+// Direct to Route Handlers
+app.use('/api/Announcement', Announcements)
 
 
 
-// Direct routes to appropriate files 
+app.use((req,res) => res.status(404).send(`<h1>Can not find what you're looking for</h1>`))
 
-app.use('/api/Carts', Carts)
-app.use('/api/RecruitmentForms', RecruitmentForms)
-app.use('/api/Admins', Admins)
-
-// Handling 404
-app.use((req, res) => {
-    res.status(404).send({err: 'We can not find what you are looking for'});
- })
-
-const port = 3000
-app.listen(port, () => console.log(`Server up and running on port ${port}`))
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server on ${port}`))
