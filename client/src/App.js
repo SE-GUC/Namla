f201c8a163cc684262ac7d144aac69e442622129
SeifYasser5 <seifyasser@icloud.com>
2019-04-08 04:53:50 +0200

    Sprint4

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,14 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import Form from './components/Form.js';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Child from './components/child'
 
 class App extends Component {
-
-  state = {
-    fields: {}
-  };
-
-  onChange = updatedValue => {
-    this.setState({
-      fields: {
-        ...this.state.fields,
-        ...updatedValue
-      }
-    });
-  };
-
   render() {
     return (
       <div className="App">
-      <div>Recruitment Form</div>
-        <Form onChange={fields => this.onChange(fields)}/>
-        <p>
-          {JSON.stringify(this.state.fields,null,2)}
-        </p>
+        <Router>
+          <Route path="/child" component={Child}/>
+        </Router>
       </div>
     );
   }
