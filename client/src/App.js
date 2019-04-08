6a0a7f7c7b7f5b39b0a67a81cece8d1128336c95
SeifYasser5 <seifyasser@icloud.com>
2019-04-08 04:18:21 +0200

    Sprint4

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import suggestionbox from './components/suggestionbox '
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Child from './components/child'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <Route path="/child" component={Child}/>
+        </Router>
       </div>
     );
   }
