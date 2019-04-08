18605325f1519a05372dcb057021ce39d536d2cd
alynamlaa <alynamlaa@gmail.com>
2019-04-08 05:11:58 +0200

    faq

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import Faqsection from './components/Faqsection'
+
 import './App.css';
-import Form from './components/Form.js';
+
 
 
 class App extends Component {
-
-  state = {
-    fields: {}
-  };
-
-  onChange = updatedValue => {
-    this.setState({
-      fields: {
-        ...this.state.fields,
-        ...updatedValue
-      }
-    });
-  };
-
   render() {
     return (
       <div className="App">
-      <div>Recruitment Form</div>
-        <Form onChange={fields => this.onChange(fields)}/>
-        <p>
-          {JSON.stringify(this.state.fields,null,2)}
-        </p>
+      <Faqsection />
+        
       </div>
     );
   }
