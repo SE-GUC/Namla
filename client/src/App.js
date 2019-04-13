aaec55db8ac649a98898e52f116aed5bf2154b04
marwanelshantaly <marwan.elshantaly@gmail.com>
2019-04-13 15:53:31 +0200

    Delete and mark as read FAQ
    added the delete and mark as read reacts to the FAQ section

--- client/src/App.js
+++ client/src/App.js
@@ -17,15 +17,18 @@ class App extends Component {
     Faqsection2 : [
       {
         id:uuid.v4(),
-        title: 'Read the questions carefully.'
+        title: 'Read the questions carefully.',
+        read:false
       },
       {
         id:uuid.v4(),
-        title: 'Number of commities?  Answer:6'
+        title: 'Number of commities?  Answer:6',
+        read:false
       },
       {
         id:uuid.v4(),
-        title: 'Working Hours?     Answer:8'
+        title: 'Working Hours?     Answer:8',
+        read: false
       }
     ]
   }
@@ -37,6 +40,25 @@ class App extends Component {
     }
     this.setState({Faqsection2 : [...this.state.Faqsection2,newFaqsection2]});
   }
+
+   markRead = (id) =>{
+     this.setState( { Faqsection2: this.state.Faqsection2.map( Faqsection2 => {
+     if(Faqsection2.id == id){
+       Faqsection2.read = !Faqsection2.read
+
+     }  
+
+     return Faqsection2;
+
+
+     } ) } );
+   }
+
+   delFaqsection2 =(id) =>{
+     this.setState({ Faqsection2:[...this.state.Faqsection2.filter(Faqsection2 => Faqsection2.id 
+      !== id )] });
+   }
+    
   render() {
     return (
       <div className="App">
@@ -63,7 +85,9 @@ class App extends Component {
       <div className="container">
       <Header />
       <AddFAQ AddFAQ={this.AddFAQ} />
-      <Faqsection2 Faqsection2={this.state.Faqsection2} />
+      <Faqsection2 Faqsection2={this.state.Faqsection2} markRead = {this.markRead} 
+       delFaqsection2 = {this.delFaqsection2}
+      />
       </div>
       </div>
       </div>
--- client/src/components/Faqsection2.js
+++ client/src/components/Faqsection2.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import Faqsectionitem from './Faqsectionitem';
 import PropTypes from 'prop-types';
 class Faqsection2 extends Component {
+ 
+ 
+ 
   render() {
     return this.props.Faqsection2.map((Faqsection2) => (
-      <Faqsectionitem key={Faqsection2.id} Faqsection2 ={Faqsection2}/>
+      <Faqsectionitem key={Faqsection2.id} Faqsection2 ={Faqsection2} markRead= {this.props.markRead}
+      delFaqsection2 = {this.props.delFaqsection2}
+      
+      />
 
 
     ));
