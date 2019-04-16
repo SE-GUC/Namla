96d882b58fefebfe97946f6aad6835b29e5b74e6
AyaSeif <aya.bayoumy@student.guc.edu.eg>
2019-04-16 02:42:37 +0200

    updatesprint5

--- tests/Anno.fns.js
+++ tests/Anno.fns.js
@@ -3,24 +3,24 @@ axios.defaults.adapter= require("axios/lib/adapters/http")
 const announcement={
     createAnnouncement:async(data) => {
    
-        const response =await axios.post('http://localhost:5000/api/Announcement/',data);
+        const response =await axios.post('http://localhost:3000/api/Announcement/',data);
         return response;
     },
 
     getAnnouncement:async() => {
-        const response =await axios.get('http://localhost:5000/api/Announcement/');
+        const response =await axios.get('http://localhost:3000/api/Announcement/');
          return response;
     },
 
     updateAnnouncement:async(id,data) => {
-         const response =await axios.put(`http://localhost:5000/api/Announcement/${id}`,data);
+         const response =await axios.put(`http://localhost:3000/api/Announcement/${id}`,data);
          return response;
      },
    
 
     deleteAnnouncement:async(id) => {
        
-         const response =await axios.delete(`http://localhost:5000/api/Announcement/${id}`);
+         const response =await axios.delete(`http://localhost:3000/api/Announcement/${id}`);
          return response;
      },
 }
