acf0ca723143aba245b1442b4c05e1385b9bdca3
SeifYasser5 <seifyasser@icloud.com>
2019-04-23 04:55:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const gallery = require('./routes/api/gallery')
 const db = require('./config/keys').mongoURI
 
 mongoose
-    .connect('mongodb://localhost:27017/namla')
+    .connect(db)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err))
 
