4a8ab5d4f26406ce87028771980eeafd4364e692
marwanelshantaly <marwan.elshantaly@gmail.com>
2019-04-23 05:49:57 +0200

    Update Header.js

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -4,7 +4,7 @@ import{Link} from 'react-router-dom'
 export default function HeaderAnnoun() {
   return (
     <header style={headerStyle}>
-      <h1>ANNOUNCEMENTS</h1>
+      <h1>FAQ</h1>
       <Link style={linkStyle} to="/">FAQ</Link>|<Link style={linkStyle} to="/about">About</Link>
       </header>
   )
